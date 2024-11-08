c46cfa55bc36f4347c72bc26f66b70b247aa9fc5
aylingumus <aylingumus7@gmail.com>
2024-11-08 16:52:57 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/smartenergysimulator/SmartEnergySimulatorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.smartenergysimulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartEnergySimulatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmartEnergySimulatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/smartenergysimulator/SmartEnergySimulatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.smartenergysimulator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartEnergySimulatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
