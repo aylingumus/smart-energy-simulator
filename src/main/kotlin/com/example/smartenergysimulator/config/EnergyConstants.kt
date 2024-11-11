061b96f2b3e92f055350785ac9e875494b2e6094
aylingumus <aylingumus7@gmail.com>
2024-11-11 17:10:49 +0100

    Create a class for energy constants

---
+++ src/main/kotlin/com/example/smartenergysimulator/config/EnergyConstants.kt
@@ -0,0 +1,11 @@
+package com.example.smartenergysimulator.config
+
+object EnergyConstants {
+    const val BATTERY_EFFICIENCY = 0.9
+    const val DAYLIGHT_START = 6
+    const val DAYLIGHT_END = 18
+    const val MIN_TEMPERATURE = 15.0
+    const val MAX_TEMPERATURE = 25.0
+    const val TARGET_TEMPERATURE = 20.0
+    const val LOW_BATTERY_THRESHOLD = 0.1 // 10% of capacity
+}
